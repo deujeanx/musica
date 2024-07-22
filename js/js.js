2490e492b9549caa32c9968899eff6484bf9cd28
deujeanx <jeansbanguera@gmail.com>
2024-07-21 22:22:53 -0500

    hice que las canciones se pudieran reproducir y casi que termine la pagina principal

---
+++ js/js.js
@@ -0,0 +1,20 @@
+document.getElementById('toggleicon').addEventListener('click', function() {
+    this.classList.toggle('fa-circle-pause');
+    this.classList.toggle('fa-circle-play');
+});
+
+document.getElementById('toggleicon').addEventListener('click', function() {
+
+    var audio = document.getElementById('cancion');
+
+    if (audio.paused){
+
+        audio.play();
+
+    } else {
+
+        audio.pause();
+
+    }
+
+})
